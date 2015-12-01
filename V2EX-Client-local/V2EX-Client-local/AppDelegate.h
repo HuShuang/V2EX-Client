0d28d943195863f9ddb4dc561e4b7177b83164f7
hushuang <hushuangg@outlook.com>
2015-12-01 10:06:52 +0800

    v1.新增了功能

---
+++ V2EX-Client-local/V2EX-Client-local/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  V2EX-Client-local
+//
+//  Created by tarena on 12/1/15.
+//  Copyright © 2015 hushuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ V2EX-Client-local/V2EX-Client-local/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  V2EX-Client-local
+//
+//  Created by tarena on 12/1/15.
+//  Copyright © 2015 hushuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
